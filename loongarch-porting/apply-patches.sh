0e7b846b574f45ce97bbc0214edf2a7fadd6a32a
KFERMercer <KFER.Mercer@gmail.com>
2023-06-11 14:16:05 +0800

    tools: update script

--- loongarch-porting/apply-patches.sh
+++ loongarch-porting/apply-patches.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-for i in $(cat ./debian/patches/series);
-do
-  patch -p1 -N --verbose < ./debian/patches/$i
+for i in $(cat ./debian/patches/series); do
+
+    patch -p1 -N --verbose --reject-file=/dev/null < ./debian/patches/$i
+
+    # PATCH_STAT=$?
+
+    if [[ $? != '0' ]]; then
+
+    echo "检测到合并冲突, break!!"
+
+    break
+
+    return 1
+
+    fi
+
 done
 
-exit 0
+# if [[ $PATCH_STAT != '0' ]]; then
+
+#     # for i in $(tac ./debian/patches/series); do
+
+#     #     patch -Rp1 -N --verbose --reject-file=/dev/null < ./debian/patches/$i
+#     #     patch -Rp1 -N --verbose --reject-file=/dev/null < ./debian/patches/$i
+#     #     patch -Rp1 -N --verbose --reject-file=/dev/null < ./debian/patches/$i
+
+#     # done
+
+#     return 1
+
+# fi
